f7dce626f27ea5462ff8e67028a1571a98f93e3d
Pebloxy <pabvnoob2.0@gmail.com>
2026-09-17 20:15:51 -0300

    Exercício 9 feito

--- Exercício 9.c
+++ Exercício 9.c
@@ -8,22 +8,24 @@ imprima "Erro: Dinheiro insuficiente" (similar à lógica de divisão segura). C
 
 int main() {
     int valorCompra = 0;
-    int valorComDesconto = 0;
- 
- printf( "Olá seja bem vindo ao nosso site.\n" );
- printf( "Digite o valor total de sua compra e ganhe um desconto: " );
- 
- scanf( "%d", &valorCompra );
- 
- if (valorCompra < 0){
-    printf("Digite um valor válido \n");
- } else if (valorCompra >= 100){
-    valorComDesconto = (valorCompra - 10);
-     printf("Você ganhou um desconto de R$10 \n");
-     printf("Valor total: R$%d\n", valorComDesconto);
- } else{
-     printf("Desconto não disponível \n");
-     printf("Valor total: R$%d\n", valorCompra);
- }
+    int valorPago = 0;
+    int troco = 0;
+    printf("Digite o valor total da compra: R$ ");
+    scanf("%d", &valorCompra);
+    do {
+        printf("Digite o valor fornecido pelo cliente: R$ ");
+        scanf("%d", &valorPago);
+        if (valorPago < valorCompra) {
+            printf("Erro: Dinheiro insuficiente! Falta R$ %d.\n", valorCompra - valorPago);
+            printf("Por favor, informe um valor maior ou igual a R$ %d.\n\n", valorCompra);
+        }
+    } while (valorPago < valorCompra);
+    troco = valorPago - valorCompra;
+    if (troco == 0) {
+        printf("Pagamento exato! Não há troco a devolver.\n");
+    } else {
+        printf("Troco a ser devolvido: R$ %d\n", troco);
+    }
+    printf("\nPrograma encerrado!\n");
     return 0;
 }
